c52ff296576bdd832c7ba67267d0c491a4763780
Liza <lizininua2001@gmail.com>
2026-09-12 22:12:54 +0400

    Set global npm cache in test and build shared library functions

--- vars/buildApp.groovy
+++ vars/buildApp.groovy
@@ -1,4 +1,5 @@
 def call() {
     sh 'chmod +x scripts/build.sh'
+    sh 'npm config set cache /tmp/.npm-cache --global'
     sh './scripts/build.sh'
 }
--- vars/testApp.groovy
+++ vars/testApp.groovy
@@ -1,4 +1,5 @@
 def call() {
     sh 'chmod +x scripts/test.sh'
+    sh 'npm config set cache /tmp/.npm-cache --global'
     sh './scripts/test.sh'
 }
